4f65fd191b7614eae399188a281462b261905b4f
Simon Pedersen <blasimtex@hotmail.com>
2016-03-08 22:05:52 +0100

    initial

---
+++ Commands.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJKP.ODataToTypeScript
+{
+    class Commands
+    {
+        [Option('u', "Uri", Required = true)]
+        public string MetadataDocumentUri { get; set; }
+
+        [Option('n', "Namespace", Required = true)]
+        public string NamespacePrefix { get; set; }
+
+        [Option('d', "UseDataServiceCollection", DefaultValue = true)]
+        public bool UseDataServiceCollection { get; set; }
+
+        [Option('a', "EnableNamingAlias", DefaultValue = true)]
+        public bool EnableNamingAlias { get; set; }
+
+        [Option('i', "IgnoreUnexpectedElementsAndAttributes", DefaultValue = true)]
+        public bool IgnoreUnexpectedElementsAndAttributes { get; set; }
+
+        [Option('o', "Output", DefaultValue = "output.d.ts")]
+        public string Output { get; set; }
+
+        [Option('c', "CSharp Output", DefaultValue = "class.cs")]
+        public string CsharpOutput {get;set;}
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
